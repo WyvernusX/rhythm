ab021841d1121f048ce182e9f465ca5fcb1a4bed
wyv <wyvernusx@gmail.com>
2026-02-10 11:02:25 -0500

    Edited game.lua and added a song chart system

--- game.lua
+++ game.lua
@@ -7,24 +7,28 @@ function game:enter()
     self.notes = {}
     screenwidth, screenheight = love.graphics.getDimensions()
     
-    x = screenwidth / 2
-    y = screenheight / 2
+    self.centerY = screenheight / 2 
     
     love.graphics.setLineStyle("rough")
     love.graphics.setLineWidth(20)
     love.window.setTitle("rhythm by wyv")
-
+    
     self.speed = 200
-    
     self.line_x = 110 
+    self.spawn_x = screenwidth + 100
+
+    local distance = self.spawn_x - self.line_x
+    self.travelTime = distance / self.speed
     
-    self:spawnNote("first", 0)
+    self.chart = require("songs/song1") 
+    self.chartIndex = 1 
+    self.musicTime = 0  
 end
 
 function game:spawnNote(type, offset)
     local note = {
-       xc = screenwidth + 100, 
-       yc = (y / 2) + offset, 
+       xc = self.spawn_x, 
+       yc = self.centerY + offset, 
        type = type,
        active = true 
     }
@@ -32,11 +36,28 @@ function game:spawnNote(type, offset)
 end
 
 function game:update(dt)
-    for i, note in ipairs(self.notes) do
+    if playing then 
+        self.musicTime = test1:tell()
+    end    
+
+    while self.chartIndex <= #self.chart do
+        local nextNote = self.chart[self.chartIndex]
+        
+        local spawnTime = nextNote.time - self.travelTime
+
+        if self.musicTime >= spawnTime then
+            self:spawnNote(nextNote.type, 0)   
+            self.chartIndex = self.chartIndex + 1
+        else
+            break
+        end
+    end
+
+    for i, note in ipairs(self.notes) do --speed
         note.xc = note.xc - (self.speed * dt)
     end
 
-    for i = #self.notes, 1, -1 do --the missed note remove here!
+    for i = #self.notes, 1, -1 do --remove missed notes
         if self.notes[i].xc < -50 then
             table.remove(self.notes, i)
             print("Missed note (went off screen)")
@@ -47,13 +68,20 @@ end
 function game:draw()
     love.graphics.setFont(tiny)
     love.graphics.setBackgroundColor(0.2, 0.2, 0.2)
+    
     love.graphics.setColor(1, 1, 1)
     love.graphics.rectangle("fill", 100, screenheight / 2 - 250, 20, 500)
-    love.graphics.print("Press F to hit!", 10, 10)
-    for _, note in ipairs(self.notes) do
+    love.graphics.print("f = red, j = blue", 10, 10)
+    
+    for _, note in ipairs(self.notes) do --note maker
         if note.active then
-            love.graphics.setColor(0.5, 0.5, 1) 
-            love.graphics.circle("fill", note.xc, note.yc, 30)
+            if note.type == "normalf" then
+                love.graphics.setColor(1, 0.5, 0.5) --red
+                love.graphics.circle("fill", note.xc, note.yc, 30) 
+            elseif note.type == "normalj" then
+                love.graphics.setColor(0.5, 0.5, 1) --blue
+                love.graphics.circle("fill", note.xc, note.yc, 30) 
+            end
         end
     end
     love.graphics.setColor(1, 1, 1)
@@ -61,6 +89,30 @@ end
 
 function play1()
     love.audio.play(test1)
+end
+
+function game:checkHit(keyType)
+    local closestDist = 999999
+    local closestIndex = -1
+    local hitWindow = 50 
+
+    for i, note in ipairs(self.notes) do
+        if note.type == keyType then
+            local dist = math.abs(note.xc - self.line_x)
+            
+            if dist < closestDist then
+                closestDist = dist
+                closestIndex = i
+            end
+        end
+    end
+
+    if closestIndex ~= -1 and closestDist <= hitWindow then
+        table.remove(self.notes, closestIndex)
+        print("HIT " .. keyType)
+    else
+        print("MISS " .. keyType)
+    end
 end
 
 function game:keypressed(key)
@@ -84,30 +136,10 @@ function game:keypressed(key)
         end
     end
 
-
     if key == "f" then
-        local closestDist = 999999
-        local closestIndex = -1
-        local hitWindow = 50 
-
-
-        for i, note in ipairs(self.notes) do
-            local dist = math.abs(note.xc - self.line_x)
-            
-            if dist < closestDist then
-                closestDist = dist
-                closestIndex = i
-            end
-        end
-
-
-        if closestIndex ~= -1 and closestDist <= hitWindow then
-            --success!!
-            table.remove(self.notes, closestIndex)
-            self:spawnNote("random", 0) 
-        else
-            --miss go here
-        end
+        self:checkHit("normalf")
+    elseif key == "j" then
+        self:checkHit("normalj")
     end
 end
 
---
+++ songs/song1.lua
@@ -0,0 +1,4 @@
+return {
+    {time = 2, offset = 0, type = "normalf"},
+    {time = 2.5, offset = 100, type = "normalj"}
+}
