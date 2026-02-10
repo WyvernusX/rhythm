cfb4c6f830570077ff63974d052961531e4a4637
wyv <wyvernusx@gmail.com>
2026-02-10 16:59:15 -0500

    Charted more parts of the first song

--- songs/song1.lua
+++ songs/song1.lua
@@ -4,15 +4,31 @@ vah:play()
 return { --f is red, j is blue
     bpm = 150,
     notes = {
-        {time = 2, offset = 200, type = "normalj"},
-        {time = 5, offset = 200, type = "normalj"},
-        {time = 7.5, offset = 200, type = "normalj"},
-        {time = 10, offset = 200, type = "normalj"},
+        {time = 11.5, offset = 200, type = "normalj"},
         {time = 12.5, offset = 200, type = "normalj"},
+        {time = 13.5, offset = 200, type = "normalj"},
         {time = 15, offset = 200, type = "normalj"},
+        {time = 16, offset = 200, type = "normalj"},
         {time = 17.5, offset = 200, type = "normalj"},
+        {time = 18.5, offset = 200, type = "normalj"},
+        {time = 17, offset = -200, type = "normalf"},
+        {time = 20, offset = -80 , type = "normalf"},
+        {time = 21, offset = -140 , type = "normalf"},
+        {time = 22, offset = -80, type = "normalf"},
+        {time = 24, offset = -200, type = "normalj"}, --potenital specials these two below!!
+        {time = 25, offset = -140, type = "normalj"},
+        {time = 27, offset = -80, type = "normalf"},
+        {time = 28, offset = -140, type = "normalf"},
+        {time = 29, offset = -80, type = "normalf"},
+        {time = 30, offset = -140, type = "normalf"},
+        {time = 31, offset = -80, type = "normalj"},
+        {time = 31.5, offset = -140, type = "normalj"},
+        {time = 32.5, offset = -80, type = "normalj"},
+
+
     }
 }
+
 
 --negative is up, down is positive
 --2.5 beats per second :o
