02ddf702041fda1d089625309ba3f0dd9bdec936
wyv <wyvernusx@gmail.com>
2026-02-25 10:15:02 -0500

    Started creating second song, started making settings menu

--- rhythm/settings.lua
+++ rhythm/settings.lua
@@ -2,6 +2,7 @@ local settings = {}
 setting_music = love.audio.newSource("assets/settings.mp3", "static")
 setting_music:setLooping(true)
 setting_music:play()
+scrollspeed = 2
 
 function settings:draw()
     love.graphics.setFont(small)
@@ -13,6 +14,12 @@ function settings:draw()
     love.graphics.print("scroll speed", 0, 100) --//TODO add buttons eventually
     love.graphics.print("developer mode", 0, 150)
     love.graphics.print("keybinds", 0, 200) --//TODO  button with listener i think
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.print(scrollspeed, 280, 100)
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.rectangle("fill", 220, 100, 40, 40)
+    love.graphics.rectangle("fill", 280, 100, 60, 40)
+    love.graphics.rectangle("fill", 360, 100, 40, 40)
 end
 
 function settings:keypressed(key) 
@@ -23,6 +30,17 @@ function settings:keypressed(key)
     elseif key == "f" then
         local isFullscreen = love.window.getFullscreen()
         love.window.setFullscreen(not isFullscreen, "desktop")
+    end
+end
+
+function settings:mousepressed(mouseX, mouseY, button, istouch)
+    love.audio.play(click) 
+    if mouseX >= x - 220 and mouseX <= x + 220 and
+       mouseY >= y - 100 and mouseY <= y then
+        scrollspeed = scrollspeed + 0.1
+    elseif mouseX >= 280 and mouseX <= x + 280 and
+        mouseY >= y -100 and mouseY <= y then
+        scrollspeed = scrollspeed - 0.1
     end
 end
 
---
+++ rhythm/songs/song2.lua
@@ -0,0 +1,10 @@
+song = love.audio.newSource("assets/song1.mp3", "static")
+song:play()
+song1duration = song:getDuration()
+
+return {
+    bpm = 67,
+    notes = [
+        --imagine shits here
+    ]
+}
